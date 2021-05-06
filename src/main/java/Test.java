0c851e30581813166d27a190b4a4aca124532f4e
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-05-06 13:18:12 +0300

    Base

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,19 +4,11 @@ import java.sql.*;
 public class Test {
     public static void main(String[] args) throws SQLException {
 
-       SweetnessesDao sweetnessesDao = new SweetnessesDao(DriverManager.getConnection("jdbc:mysql://localhost/MyBase", "root", "6706598"));
+        SweetnessesDao sweetnessesDao = new SweetnessesDao(DriverManager.getConnection("jdbc:mysql://localhost/MyBase", "root", "6706598"));
 
-//       System.out.println(sweetnessesDao.findByName("баунти"));
-//
-       //Sweetness sweetness = new Sweetness("БОБ", 33,10000);
-       //sweetnessesDao.create(sweetness);
+        //Sweetness sweetness = new Sweetness("БОБ", 33,10000);
+        // Sweetness sweetness = new Sweetness("Баунти",500,1500);
         System.out.println(sweetnessesDao.findAll());
-//        Sweetness sweetness = new Sweetness("Баунти",500,1500);
-//        sweetnessesDao.update(sweetness);
-//        System.out.println(sweetnessesDao.update(sweetness));
-
-
-
     }
 
 }
